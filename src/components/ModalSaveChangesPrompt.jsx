165f749c68d36651ca7b399e2e4fe25da28e56cb
Leon Velasco <lvelasco@intelitax.com>
2025-07-08 19:17:47 -0600

    Se completa funciones y estilos de modal antes de salir de pantalla de trabajo

--- src/components/ModalSaveChangesPrompt.jsx
+++ src/components/ModalSaveChangesPrompt.jsx
@@ -3,8 +3,12 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Save, CircleX } from 'lucide-react'
 
+import CircularProgress from '@mui/material/CircularProgress'
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined'
+
 const ModalSaveChangesPrompt = ({ isOpen, onClose }) => {
-  const [showModal, setShowModal] = useState(false) //  Controla visibilidad real del DOM
+  const [showModal, setShowModal] = useState(false)
+  const [ modalState, setModalState ] = useState('confirm') // 'confirm' | 'saving' | 'done'
 
   const navigate = useNavigate()
 
@@ -16,6 +20,7 @@ const ModalSaveChangesPrompt = ({ isOpen, onClose }) => {
 
   useEffect(() => {
     if (isOpen) {
+      setModalState('confirm')
       setShowModal(true)
     } else {
       setTimeout(() => setShowModal(false), 100) //Espera a desmontar para permitir animación
@@ -32,11 +37,18 @@ const ModalSaveChangesPrompt = ({ isOpen, onClose }) => {
   }
 
   const handleSaveAndExit = () => {
+    setModalState('saving')
+    // Simula guardado y muestra luego el modal final
+    setTimeout(() => {
+      setModalState('done')
+    }, 5000)
+  }
+
+  const handleAcceptAndClose = () => {
     onClose()
     setTimeout(() => {
-      alert('Progreso guardado')
       navigate('/', { replace: true })
-    }, 200)
+    }, 300)
   }
 
   return (
@@ -47,38 +59,78 @@ const ModalSaveChangesPrompt = ({ isOpen, onClose }) => {
       onClick={onClose}
     >
       <div
-        className="relative bg-[#F2F5F6] rounded-xl border-2 border-[#143559] shadow-2xl p-6 w-full max-w-[600px] relative"
+        className={`relative bg-[#F2F5F6] rounded-xl border-2 border-[#143559] shadow-2xl p-6 w-full transition-all duration-500 ease-in-out
+          ${modalState === 'confirm' || modalState === 'saving' ? 'max-w-[450px]' : 'max-w-[600px] h-[400px] pt-12' }`}
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-2xl font-bold text-[#143559] mb-4 text-center">Estas saliendo de la pantalla de trabajo</h2>
-
-        <p className="text-[#143559] mb-8 mt-8 text-center">
-          Si sales de esta pantalla sin guardar tu progreso se perderá tu trabajo.
-        </p>
-        <p className='text-[#143559] font-bold mb-6 mt-6 text-center'>
-           ¿Quieres guardar cambios antes de salir?
-        </p>
-        <div className='flex block justify-center gap-6'>
-          <button
-            className="flex items-center px-2 py-1 rounded-md border-2 shadow-[0_4px_12px_rgba(20,53,89,0.3)]
+        {modalState === 'confirm' && (
+          <>
+            <h2 className="text-2xl font-bold text-[#143559] mb-4 text-center">
+              Saliendo de la pantalla de trabajo
+            </h2>
+            <p className="text-[#143559] mb-8 mt-8 text-center">
+              Si sales de esta pantalla sin guardar tu progreso, se perderá tu trabajo.
+            </p>
+            <p className='text-[#143559] font-bold mb-6 mt-6 text-center'>
+              ¿Quieres guardar cambios antes de salir?
+            </p>
+            <div className='flex justify-center gap-6'>
+              <button
+                className="flex items-center px-2 py-1 rounded-md border-2 shadow-[0_4px_12px_rgba(20,53,89,0.3)]
                   text-[#143559] text-md font-bold transition-all duration-300 border-[#143559]
                   w-[160px] h-[56px] justify-center hover:bg-[#143559] hover:text-white"
-            onClick={ handleSaveAndExit }
-          >
-            <Save size={36} className='ml-2 mr-0' />
-            <span className="leading-tight">Guardar Cambios</span>
-          </button>
-          <button
-            className="flex items-center px-2 py-1 rounded-md border-2 shadow-[0_4px_12px_rgba(20,53,89,0.3)]
-                  text-red-600 text-md font-bold transition-all duration-300 border-red-600
+                onClick={handleSaveAndExit}
+              >
+                <Save size={36} className='ml-2 mr-0' />
+                <span className="leading-tight">Guardar Cambios</span>
+              </button>
+              <button
+                className="flex items-center px-2 py-1 rounded-md border-2 shadow-[0_4px_12px_rgba(20,53,89,0.3)]
+                  text-red-600 text-md font-bold transition-all duration-500 ease-in-out border-red-600
                   w-[160px] h-[56px] justify-center hover:bg-red-600 hover:text-white"
-            onClick={ handleExitWithoutSaving }
-          >
-            <CircleX size={36} className='ml-2 mr-0' />
-            <span className="leading-tight">Salir sin Guardar</span>
-          </button>
-        </div>
+                onClick={handleExitWithoutSaving}
+              >
+                <CircleX size={36} className='ml-2 mr-0' />
+                <span className="leading-tight">Salir sin Guardar</span>
+              </button>
+            </div>
+          </>
+        )}
 
+        {modalState === 'saving' && (
+          <div className="flex flex-col items-center justify-center h-[260px]">
+            <h2 className="text-2xl font-bold text-[#143559] mb-6">Aplicando Cambios</h2>
+            <div className="flex items-center justify-center">
+              <CircularProgress
+                size="75px"
+                thickness={5}
+                className="mt-10"
+                sx={{
+                  color: '#143559',
+                  animationDuration: '900ms',
+                }}
+              />
+            </div>
+          </div>
+        )}
+        {modalState === 'done' && (
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="text-2xl font-bold text-[#143559] mb-4">Cambios Aplicados</h2>
+            <div className="flex items-center justify-center  bg-white border border-[#143559] h-[200px] w-[350px] rounded-md py-6 px-12 shadow-sm text-center mb-6">
+              <p className="text-[#143559] font-medium">
+                Los cambios se guardaron exitosamente, puedes salir de la pantalla de trabajo
+              </p>
+            </div>
+            <button
+              onClick={handleAcceptAndClose}
+              className="flex items-center gap-2 px-4 py-1 rounded-md border-2 text-[#143559] border-[#143559] font-bold
+                hover:bg-[#143559] hover:text-white transition-all shadow-sm"
+            >
+              <CheckCircleOutlineOutlinedIcon fontSize="large"/>
+              Aceptar
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
